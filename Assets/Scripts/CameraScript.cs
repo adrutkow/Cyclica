4510cabf8e33d0b71195d6b510704310b406743b
adamr <adam.rutkowski9033@gmail.com>
2023-08-07 02:25:29 +0200

    Started the revamp, fighting with pixel perfect camera

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -13,23 +13,38 @@ public class CameraScript : MonoBehaviour
     public bool firstClickedNothing = false;
     public static CameraScript mainCamera;
 
-    // Start is called before the first frame update
     void Start()
     {
         cam = GetComponent<Camera>();
-        mainCamera = this;
+
+        // Calculate the desired aspect ratio (e.g., 16:9)
+        float targetAspect = 16f / 9f;
+        // Calculate the current aspect ratio
+        float currentAspect = (float)Screen.width / Screen.height;
+
+        // Calculate the viewport width and height based on the current aspect ratio and the desired aspect ratio
+        float viewportWidth = 1f;
+        float viewportHeight = 1f;
+
+        if (currentAspect < targetAspect)
+        {
+            // Add black bars on the sides
+            viewportWidth = currentAspect / targetAspect;
+        }
+        else
+        {
+            // Add black bars on the top and bottom
+            viewportHeight = targetAspect / currentAspect;
+        }
+
+        // Set the viewport rect of the Camera to add black bars if necessary
+        Camera.main.rect = new Rect((1f - viewportWidth) / 2f, (1f - viewportHeight) / 2f, viewportWidth, viewportHeight);
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
-        cam.orthographicSize -= Input.mouseScrollDelta.y;
-
-        if (cam.orthographicSize < 1) cam.orthographicSize = 1;
-        if (cam.orthographicSize > 14) cam.orthographicSize = 14;
-
-
         //Debug.Log(cam.GetComponent<PixelPerfectCamera>().pixelRatio);// += Input.mouseScrollDelta.y;
 
 
@@ -54,7 +69,20 @@ public class CameraScript : MonoBehaviour
         {
             drag = false;
         }
+/*
+        float roundedX = Mathf.Round(difference.x / 0.0625f) * 0.0625f;
+        float roundedY = Mathf.Round(difference.y / 0.0625f) * 0.0625f;
+        float roundedZ = Mathf.Round(difference.z / 0.0625f) * 0.0625f;
+        difference = new Vector3(roundedX, roundedY, roundedZ);*/
 
         if (drag) transform.position = origin - difference;
+
+        Vector3 position = Camera.main.transform.position;
+        float roundedX = Mathf.Round(position.x / 0.0625f) * 0.0625f;
+        float roundedY = Mathf.Round(position.y / 0.0625f) * 0.0625f;
+        float roundedZ = Mathf.Round(position.z / 0.0625f) * 0.0625f;
+
+        Vector3 roundedPosition = new Vector3(roundedX, roundedY, roundedZ);
+        Camera.main.transform.position = roundedPosition;
     }
 }
